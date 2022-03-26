787062a9206eadcd58b47c48800242d3b21def2c
Jesús García <jesus@pricode.io>
2022-03-26 12:57:26 -0500

    adding awards migration

---
+++ database/migrations/2022_03_26_154800_create_awards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAwardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('awards', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 125);
+            $table->string('lastname', 125);
+            $table->string('address', 125);
+            $table->string('phone', 125);
+            $table->string('email', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('awards');
+    }
+}
