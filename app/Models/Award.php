460f1e6988f5601880f8f361bf77af6570a11c3d
Jesús García <jesus@pricode.io>
2022-03-28 02:46:45 -0500

    Creating models

--- app/Models/Award.php
+++ app/Models/Award.php
@@ -9,9 +9,9 @@ class Award extends Model
     protected $fillable = [
         'name',
         'lastname',
-        'email',
         'address',
         'phone',
-        'email'
+        'email',
+        'award'
     ];
 }
