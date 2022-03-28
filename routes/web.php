cbd358cfe7a96b2e5037728241b215b65983a8fa
Jesús García <jesus@pricode.io>
2022-03-28 02:47:31 -0500

    Adding routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\AwardController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LogoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'App\Http\Controllers'], function()
+{
+    /**
+     * Dashboard Route
+     */
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    /**
+     * Awards Routes
+     */
+    //Route::resource('awards', AwardController::class);
+    Route::post('award', [AwardController::class, 'store'])->name('award');
+    Route::get('/awards', [AwardController::class, 'index'])->name('awards');
+
+    Route::group(['middleware' => ['guest']], function()
+    {
+        /**
+         * Register Routes
+         */
+        Route::get('/register', [RegisterController::class, 'show'])->name('register.show');
+        Route::post('/register', [RegisterController::class, 'register'])->name('register');
+
+        /**
+         * Login Routes
+         */
+        Route::get('/login', [LoginController::class, 'show'])->name('login.show');
+        Route::post('/login', [LoginController::class, 'login'])->name('login');
+    });
+
+    Route::group(['middleware' => ['auth']], function()
+    {
+        /**
+         * Logout Routes
+         */
+        Route::get('/logout', [LogoutController::class, 'logout'])->name('logout');
+    });
+
 });
