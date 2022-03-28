81822afd387c4059eebbbb38942f6d6f574e31b7
Jesús García <jesus@pricode.io>
2022-03-28 02:45:45 -0500

    Creating request for validations

---
+++ app/Http/Requests/AwardRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class AwardRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'string|required|max:100',
+            'lastname' => 'nullable|string|max:100',
+            'address' => 'nullable|string|max:100',
+            'phone' => 'nullable|string|max:11',
+            'email' => 'email:rfc,dns',
+        ];
+    }
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'errors' => $validator->errors(),
+            'status' => true
+        ], 422));
+    }
+
+}
